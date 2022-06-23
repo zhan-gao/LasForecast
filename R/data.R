c353dc403c3b52f82ae46d9152604ff43735aa6f
Dai Qiyu <83342930+daiqiyu890@users.noreply.github.com>
2022-06-23 12:03:22 +0800

    adding documentation for the real example data

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' Growth Rate of US industry Production Growth.
+#'
+#' A monthly dataset containing the growth rate of US industry production growth rate
+#' and other macroeconomic variables from 1960-01-01 to 2022-04-01
+#'
+#' @format A data frame with 748 rows and 110 variables:
+#' \describe{
+#'   \item{Ind_Growth_Rate}{monthly growth rate of US Industrial Production Index}
+#'   \item{RPI}{real personal income}
+#'   \item{W875RX1}{real personal income ex transfer receipts}
+#'   ...
+#' }
+#' @source Industrial Production Index \url{https://fred.stlouisfed.org/series/INDPRO}
+#'
+#' Remaining Macroeconomic Factors \url{https://research.stlouisfed.org/econ/mccracken/fred-databases/}
+"US_industry_prod"
