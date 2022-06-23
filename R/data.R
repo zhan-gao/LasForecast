6e086f62c0b686dec87fd4bf494f211b7f69719c
Dai Qiyu <83342930+daiqiyu890@users.noreply.github.com>
2022-06-23 17:52:22 +0800

    modify Rd for data

--- R/data.R
+++ R/data.R
@@ -1,7 +1,7 @@
 #' Growth Rate of US industrial Production Index.
 #'
-#' A monthly dataset containing the growth rate of US industrial production
-#' and other macroeconomic variables from 1960-01-01 to 2022-04-01
+#' A monthly dataset containing the growth rate of US Industrial Production Index
+#' and other macroeconomic variables from 1960-01-01 to 2022-04-01.
 #'
 #' @format A data frame with 748 rows and 110 variables:
 #' \describe{
@@ -12,5 +12,5 @@
 #' }
 #' @source Industrial Production Index \url{https://fred.stlouisfed.org/series/INDPRO}
 #'
-#' Remaining Macroeconomic Factors \url{https://research.stlouisfed.org/econ/mccracken/fred-databases/}
+#' Other Macroeconomic Factors \url{https://research.stlouisfed.org/econ/mccracken/fred-databases/}
 "US_industry_prod"
